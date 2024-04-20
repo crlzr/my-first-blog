911ab7850110f8e40cf29ddc555c96b85410f234
Carole <c.zenruffinen@hotmail.com>
2024-04-20 15:39:41 +1000

    updated files

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
+    # Retrieve all posts that are published and order them by published date
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    # Retrieve the post with the given pk (primary key) or return a 404 error if not found
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
